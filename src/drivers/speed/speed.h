f4552658613607a35e240147d6fd8bedd893fbe0
PandurangHegde <67312979+PandurangHegde@users.noreply.github.com>
2021-05-17 05:44:57 +0200

    Update speed.h

--- src/drivers/speed/speed.h
+++ src/drivers/speed/speed.h
@@ -3,14 +3,14 @@
  */
 
 /** @brief Sets the speed to the input value
- *  @param[out] value to which shall be set as speed
+ *  @param[in] speed Value to which shall be set as speed
  *  @note This is just some code for manual mocking that can help with
  *    testing!
  */
 void set_speed(float speed);
 
 /** @brief Returns the current speed
- *  @param[out] current speed
+ *  @param[out] curSpeed Current speed of the vehicle
  */
 float get_speed();
 
